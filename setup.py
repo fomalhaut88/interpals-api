681b9a85732d0cdae15d9e1562f4f93eb3157282
Alexander X <alexfomalhaut@gmail.com>
2025-07-26 18:05:51 +0200

    Update Python version.

--- setup.py
+++ setup.py
@@ -23,12 +23,12 @@ setup(
     zip_safe=False,
     long_description=get_long_description(),
     long_description_content_type="text/markdown",
-    python_requires='>=3.8',
+    python_requires='>=3.10',
     install_requires=[
-        'aiohttp>=3.8',
-        'requests>=2.31',
-        'lxml>=4.9',
-        'beautifulsoup4>=4.12',
+        'aiohttp>=3.12',
+        'requests>=2.32',
+        'lxml>=6.0',
+        'beautifulsoup4>=4.13',
     ],
     classifiers=[
         "Development Status :: 5 - Production/Stable",
